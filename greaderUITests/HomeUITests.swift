61a5945504667d92e64801c3e8436638112de840
Lohan Marques <lohanmsantos@gmail.com>
2020-01-05 21:36:49 -0300

    inicio dos testes de ui

--- greaderUITests/HomeUITests.swift
+++ greaderUITests/HomeUITests.swift
@@ -2,13 +2,25 @@
 
 import XCTest
 
-class greaderUITests: XCTestCase {
+class HomeUITests: XCTestCase {
+    var app: XCUIApplication!
 
     override func setUp() {
-        
+        app = XCUIApplication()
+        app.launch()
     }
 
     override func tearDown() {
+        app = nil
+    }
+    
+    func testEnterArticleDetailsWithContent() {
+        let cell = app.tables.element(boundBy: 0).cells.element(boundBy: 0)
         
+        cell.tap()
+        
+        let content = app.textViews.matching(identifier: "articleContent")
+        
+        XCTAssertFalse(content.staticTexts["Conteúdo"].exists)
     }
 }
